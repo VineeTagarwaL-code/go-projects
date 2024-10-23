efccaf5db4df3a2b418e96e92282dae52ebc755b
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-10-23 21:26:12 +0530

    [chore]: added goask and gomon

--- go-gomon/project-test/index.js
+++
@@ -1,4 +0,0 @@
-const a = 1;
-const b = 2;
-console.log("hi");
-console.log(" answer of addition: ", a + b);
--- go-gomon/project-test/main.py
+++
@@ -1,6 +0,0 @@
-print("This is a test project for me")
-
-a  = 1
-b = 2
-c = a + b
-print("hello")
